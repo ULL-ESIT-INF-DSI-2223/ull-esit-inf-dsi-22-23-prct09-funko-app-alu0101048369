6220c660b3137bbd1650ceab5d9c69dac546fc9d
Miguel-Dorta <contact@migueldorta.com>
2023-03-30 00:26:36 +0100

    Add Funko storage and print funcs

---
+++ src/p09/funko/print.ts
@@ -0,0 +1,41 @@
+import chalk from "chalk";
+import Funko from "./funko";
+
+export function printUserFunkoCollection(user: string, funkos: Funko[]): void {
+  let maxMarketValue = funkos.reduce((max, f) => max >= f.marketValue ? max : f.marketValue, 0)
+  if (maxMarketValue === 0) {
+    maxMarketValue = Infinity
+  }
+
+  green(`${user} Funko Pop Collection`)
+  funkos.forEach(f => {
+    separator()
+    printFunko(f, maxMarketValue)
+  })
+}
+
+export function printFunko(f: Funko, maxMarketValue?: number): void {
+  green(`ID:               ${f.id}`)
+  green(`Name:             ${f.name}`)
+  green(`Description:      ${f.description}`)
+  green(`Type:             ${f.type}`)
+  green(`Genre:            ${f.genre}`)
+  green(`Franchise:        ${f.franchise}`)
+  green(`Number:           ${f.franchiseNumber}`)
+  green(`Exclusive:        ${f.exclusive ? "Yes" : "No"}`)
+  green(`Special features: ${f.specialFeatures}`)
+  green(`Market value:     ${marketValueStr(f.marketValue, maxMarketValue)} €`)
+}
+
+function marketValueStr(n: number, maxMarketValue = Infinity): string {
+  const colorOffset = Math.round((n*255)/maxMarketValue)
+  return chalk.rgb(255 - colorOffset, colorOffset, 0)(n)
+}
+
+function green(str: string): void {
+  console.log(chalk.green(str));
+}
+
+function separator(): void {
+  green("------------------------------------------------------")
+}
---
+++ src/p09/funko/storage.ts
@@ -0,0 +1,55 @@
+import fs from "node:fs";
+import path from "node:path";
+import Funko from "./funko";
+
+export default class Storage {
+  constructor(private dirPath = "funkos") {}
+
+  add(user: string, f: Funko): void {
+    fs.writeFileSync(
+      path.join(this.createUserDir(user), `${f.id}.json`),
+      JSON.stringify(f),
+      {flag: "wx"})
+  }
+
+  list(user: string): Funko[] {
+    const userDir = path.join(this.dirPath, user)
+    return fs.readdirSync(userDir).reduce((acc, filename) => {
+      if (!filename.endsWith(".json")) {
+        return acc;
+      }
+
+      acc.push(
+        Funko.parse(
+          filename.slice(0, -5),
+          JSON.parse(fs.readFileSync(path.join(userDir, filename), "utf-8"))
+        )
+      )
+      return acc;
+    }, new Array<Funko>())
+  }
+
+  read(user: string, id: string): Funko {
+    return Funko.parse(id, JSON.parse(fs.readFileSync(path.join(this.dirPath, user, `${id}.json`), "utf-8")))
+  }
+
+  remove(user: string, id: string): void {
+    fs.rmSync(path.join(this.dirPath, user, `${id}.json`))
+  }
+
+  update(user: string, id: string, fields: Map<string, unknown>): void {
+    const fPath = path.join(this.dirPath, user, `${id}.json`)
+    const rawData = JSON.parse(fs.readFileSync(fPath, "utf-8"))
+    Array.from(fields.entries()).forEach(field => rawData[field[0]] = field[1])
+    fs.writeFileSync(fPath, JSON.stringify(rawData))
+  }
+
+  private createUserDir(user: string): string {
+    if (user === "") {
+      throw new Error("user cannot be empty string");
+    }
+    const userDir = path.join(this.dirPath, user)
+    fs.mkdirSync(userDir, {recursive: true})
+    return userDir
+  }
+}
